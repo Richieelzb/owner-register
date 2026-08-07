53404b576ba66de17732ced95227e7f3050ccf7d
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-08-07 23:49:19 +0200

    fixing steps

--- static/js/main.js
+++ static/js/main.js
@@ -23,36 +23,57 @@ document
     console.log(data);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
+
+    const step1 = document.getElementById("step1");
+    const step2 = document.getElementById("step2");
+    const step3 = document.getElementById("step3");
 
     const indicator1 = document.getElementById("indicator1");
     const indicator2 = document.getElementById("indicator2");
     const indicator3 = document.getElementById("indicator3");
 
-    // Step 1 -> Step 2
-    document.getElementById("nextBtn")
-        .addEventListener("click", function () {
+    // STEP 1 -> STEP 2
+    document.getElementById("nextBtn").addEventListener("click", () => {
+
+        step1.classList.remove("active");
+        step2.classList.add("active");
 
         indicator1.classList.remove("active");
         indicator2.classList.add("active");
 
     });
 
-    // Step 2 -> Step 1
-    document.getElementById("prevBtn")
-        .addEventListener("click", function () {
+    // STEP 2 -> STEP 1
+    document.getElementById("prevBtn").addEventListener("click", () => {
+
+        step2.classList.remove("active");
+        step1.classList.add("active");
+
+        indicator2.classList.remove("active");
+        indicator1.classList.add("active");
+
+    });
+
+    // STEP 2 -> STEP 3
+    document.getElementById("nextBtn2").addEventListener("click", () => {
+
+        step2.classList.remove("active");
+        step3.classList.add("active");
 
         indicator2.classList.remove("active");
         indicator3.classList.add("active");
 
     });
 
-    // Step 2 -> Step 3
-    document.getElementById("nextBtn2")
-        .addEventListener("click", function () {
+    // STEP 3 -> STEP 2
+    document.getElementById("prevBtn2").addEventListener("click", () => {
 
-        step2.classList.remove("active");
-        step3.classList.add("active");
+        step3.classList.remove("active");
+        step2.classList.add("active");
+
+        indicator3.classList.remove("active");
+        indicator2.classList.add("active");
 
     // Populate review section
     document.getElementById("reviewData").innerHTML = `
@@ -76,11 +97,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Step 3 -> Step 2
-    document.getElementById("prevBtn2")
-        .addEventListener("click", function () {
+   document.getElementById("prevBtn2").addEventListener("click", () => {
 
         step3.classList.remove("active");
         step2.classList.add("active");
+
+        indicator3.classList.remove("active");
+        indicator2.classList.add("active");
 
     });
 
